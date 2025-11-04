d0aa00f0fe5b3cd6fcd82f5e54b4e0f50673387a
zorg <c.f.haak@posteo.de>
2025-11-04 14:36:13 +0100

    made script worse to be better

--- 05/code/f.py
+++ 05/code/f.py
@@ -1,9 +1,6 @@
 def f(n, mem={0:1, 1:1, 2:2, 3:6}):
-    """Compute factorial of n using memoization."""
     if n not in mem:
-        prev_number = n - 1
-        prev_fact = f(prev_number)
-        mem[n] = n * prev_fact
+        mem[n] = n * f(n - 1)
     return mem[n]
 
-f(10)
+print(f(10))
